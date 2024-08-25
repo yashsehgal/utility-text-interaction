7bfeb24683b26af43d79e9df582c2812ca3cd37f
Yash Sehgal <yashsehgal.work@gmail.com>
2024-08-26 05:20:37 +0530

    fix: resolve rendering issue for vercel

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,9 @@ const Home = () => {
       <div className="space-y-12">
         <div className="flex flex-row items-center justify-end w-[90%]">
           <div className="font-medium w-[36ch] text-right border-b-4 border-black border-t-2 border-x-2 p-4 rounded-xl bg-neutral-100">
-            show me all projects with their respective teammates assigned.for
-            "new year 2024 template" sprint.
+            {
+              'show me all projects with their respective teammates assigned for "new year 2024 template" sprint.'
+            }
           </div>
         </div>
         <InteractableText />
